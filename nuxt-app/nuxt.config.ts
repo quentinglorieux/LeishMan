323328132a9d6b08a9da06d3ee26d8ff7c02536e
olivierglrx <olivier.glrx@gmail.com>
2024-10-04 11:12:19 +0200

    nuxt config

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,11 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from "@primevue/themes/aura";
-import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
   ssr: true, // Enable SSR
-  compatibilityDate: '2024-04-03',
   compatibilityDate: "2024-04-03",
+
   devtools: { enabled: true },
   components: true, // Ensure auto-import for components is enabled
   modules: [
@@ -14,18 +13,16 @@ export default defineNuxtConfig({
     "@primevue/nuxt-module",
     "@pinia/nuxt",
   ],
-  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
+
   tailwindcss: {
     exposeConfig: true, // This is important for Nuxt UI to use Tailwind config
   },
   css: [
     "@/assets/css/tailwind.css",
     "primeicons/primeicons.css", // PrimeIcons
-    "@/assets/css/tailwind.css",
-    "primeicons/primeicons.css", // PrimeIcons
   ],
   nitro: {
-    preset: 'netlify' // Ensures proper configuration for SSR on Netlify
+    preset: "netlify", // Ensures proper configuration for SSR on Netlify
   },
   primevue: {
     options: {
@@ -33,16 +30,5 @@ export default defineNuxtConfig({
         preset: Aura,
       },
     },
-  },
-});
-
-    options: {
-      theme: {
-        preset: Aura,
-      },
-    },
-  },
-  runtimeConfig: {
-    ggAPIkey: process.env.API_KEY,
   },
 });
