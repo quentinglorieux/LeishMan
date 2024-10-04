84e6b1538333f211baf448bca3c039ce7abf5ed2
olivierglrx <olivier.glrx@gmail.com>
2024-10-04 11:02:38 +0200

    maps

--- nuxt-app/components/ReferenceMap.vue
+++ nuxt-app/components/ReferenceMap.vue
@@ -1,7 +1,54 @@
-<template>
-  <div class="reference-map flex justify-center mx-auto">
-    <Image src="/images/map.png" alt="Reference Centres Map" class="block" />
-  </div>
-</template>
+<script setup lang="ts">
+import { applyPlugin } from "#app";
+import {
+  GoogleMap,
+  Marker,
+  CustomMarker,
+  InfoWindow,
+  MarkerCluster,
+} from "vue3-google-map";
+const config = useRuntimeConfig();
+const apikey = config.ggAPIkey;
 
-<script setup></script>
+const center = { lat: 48.866667, lng: 2.333 };
+// const center = { lat: 52.36834, lng: 4.88635 };
+const markerOptions = { position: center, label: "L1", title: "Labo 1" };
+const isOpen = ref(false);
+const locations = [
+  { lat: 48.866667, lng: 2.333, info: "Labo 1" },
+  { lat: 48.866667, lng: 2.534, info: "Labo 2" },
+  { lat: 48.866667, lng: 2.735, info: "Labo 3" },
+  { lat: 49.866667, lng: 5.333, info: "Labo 4" },
+  { lat: 42.866667, lng: 1.333, info: "Labo 5" },
+  { lat: 41.866667, lng: -1.333, info: "Labo 6" },
+  { lat: 43.866667, lng: 5.333, info: "Labo 7" },
+  { lat: 40.866667, lng: 2.233, info: "Labo 8" },
+];
+</script>
+<template>
+  <div>{{ apikey }}</div>
+  <GoogleMap
+    :api-key="apikey"
+    style="width: 100%; height: 500px"
+    :center="center"
+    :zoom="5"
+  >
+    <Marker
+      v-for="(location, i) in locations"
+      :key="i"
+      :options="{ position: { lat: location['lat'], lng: location['lng'] } }"
+      clickable="true"
+    >
+      <InfoWindow :options="{ position: location }">
+        <InfoLabo
+          :labo="location['info']"
+          :lat="location['lat']"
+          :lng="location['lng']"
+        >
+        </InfoLabo>
+      </InfoWindow>
+    </Marker>
+
+    <!-- </MarkerCluster> -->
+  </GoogleMap>
+</template>
--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,27 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import Aura from '@primevue/themes/aura';
+import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   components: true, // Ensure auto-import for components is enabled
-  modules: [
-    '@nuxt/ui',
-    '@nuxtjs/tailwindcss',
-    '@primevue/nuxt-module'
-  ],
+  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
   tailwindcss: {
     exposeConfig: true, // This is important for Nuxt UI to use Tailwind config
   },
   css: [
-    '@/assets/css/tailwind.css',
-    'primeicons/primeicons.css', // PrimeIcons
+    "@/assets/css/tailwind.css",
+    "primeicons/primeicons.css", // PrimeIcons
   ],
   primevue: {
-      options: {
-          theme: {
-              preset: Aura
-          }
-      }
-  }
-})
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+  runtimeConfig: {
+    ggAPIkey: process.env.API_KEY,
+  },
+});
