24d878e39bdc8367dcadc1442e9e211be2a72e7d
Quentin Glorieux <quentin.glorieux@gmail.com>
2024-10-06 13:06:42 +0200

    update 2 files and delete 1 file

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,19 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true, // Enable or Disable SSR
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  components: true, // Ensure auto-import for components is enabled
+  components: true, 
   modules: ["@nuxt/ui", "@pinia/nuxt"],
 
   tailwindcss: {
     exposeConfig: true, 
   },
 
-  nitro: {
-  },
-
   runtimeConfig: {
     gmapsApiKey: process.env.GOOGLE_MAPS_API_KEY || 'wrong_key', 
+    public: {
+      publicApiKey: process.env.GOOGLE_MAPS_API_KEY || 'public' 
+    }
   }
 
 });
