14d819e35bb50413e50ee93a520bc0373b5aa754
Quentin Glorieux <quentin.glorieux@gmail.com>
2024-10-15 23:38:28 +0200

    build(deps): update package-lock.json, package.json, publications.vue and committee.json

--- nuxt-app/pages/publications_open.vue
+++ nuxt-app/pages/publications_open.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mx-auto px-4 pt-10 dark:bg-slate-800">
-    <Titleheader title="  Publications" />
+  <div class="mx-auto px-4 bg-white dark:bg-slate-800">
+    <Titleheader title="Full List of Publications" />
     <!-- Year Filter -->
     <div class="flex flex-wrap space-x-2 mb-6">
       <button
@@ -31,13 +31,12 @@
       v-for="(works, year) in filteredPublications"
       :key="year"
     >
-
       <h2 class="text-2xl font-semibold mt-6">{{ year }}</h2>
       <ul>
-        <li v-for="work in works" :key="work.pmid" class="mb-4">
+        <li v-for="work in works" :key="work.id" class="mb-4">
           <div class="text-xl font-semibold">
             <a
-              :href="'https://pubmed.ncbi.nlm.nih.gov/' + work.pmid"
+              :href="work.primary_location.landing_page_url"
               target="_blank"
               class="hover:underline text-black dark:text-gray-300"
             >
@@ -45,18 +44,46 @@
             </a>
           </div>
           <div class="text-gray-600 mb-2 dark:text-gray-300">
-            <span v-for="(author, index) in work.authors" :key="author" class="mr-2">
-  {{ author }}<span v-if="index < work.authors.length - 1">,</span>
-</span>
+            <span
+              v-for="author in work.authorships"
+              :key="author.author.id"
+              class="mr-2"
+            >
+              {{ author.author.display_name }}
+              <a
+                :href="author.author.orcid"
+                target="_blank"
+                v-if="author.author.orcid"
+                class="inline-block align-middle ml-1"
+              >
+                <img
+                  src="https://orcid.org/sites/default/files/images/orcid_24x24.png"
+                  alt="ORCID Logo"
+                  class="w-3 h-3 align-text-top"
+                  style="position: relative; top: -0.5em; left: -0.4em"
+                />
+              </a>
+            </span>
           </div>
 
-
-
-          <div v-if="work.doi" class="text-sm text-blue-500">
-            
-            <a :href="'https://doi.org/' + work.doi" target="_blank" class="">
-              <span>{{ work.journal }},  </span>
-              <span> <strong> {{ work.volume }}  </strong> ({{ work.year }})</span>
+          <div class="text-blue-500">
+            <a
+              :href="work.primary_location.landing_page_url"
+              target="_blank"
+              class="hover:underline"
+            >
+              {{ work.primary_location.source.display_name }}
+            </a>
+            <span>
+              {{ work.primary_location.volume }} ({{
+                work.publication_year
+              }})</span
+            >
+          </div>
+          <div v-if="work.open_access.is_oa" class="text-sm text-blue-500">
+            Open access:
+            <a :href="work.open_access.oa_url" target="_blank" class="ml-2">
+              <i class="pi pi-download"></i>
             </a>
           </div>
         </li>
@@ -64,7 +91,6 @@
     </div>
   </div>
 </template>
-
 
 <script setup>
 const publications = ref([]);
@@ -74,25 +100,23 @@ const filteredPublications = ref({});
 
 onMounted(async () => {
   try {
-    // Fetch the improved JSON file
-    const response = await fetch("/data/publications.json"); // Update the path accordingly
-    const publicationData = await response.json();
-    
-    // Directly use the publication data with year and authors already in the desired format
-    publications.value = publicationData;
+    const response = await fetch(
+      "https://api.openalex.org/works?filter=authorships.author.id:A5017657842&select=title,open_access,authorships,primary_location,publication_year"
+    );
+    const data = await response.json();
+    publications.value = data.results;
 
     // Get available years from the data
     const years = publications.value
-      .map((work) => work.year) // No need to extract year from date anymore
+      .map((work) => work.publication_year)
       .filter(Boolean);
     availableYears.value = [...new Set(years)].sort((a, b) => b - a);
 
     filterByYear("all");
   } catch (error) {
-    console.error("Error fetching publications data:", error);
+    console.error("Error fetching publications:", error);
   }
 });
-
 
 function filterByYear(year) {
   selectedYear.value = year;
@@ -100,7 +124,7 @@ function filterByYear(year) {
     filteredPublications.value = groupByYear(publications.value);
   } else {
     const filtered = publications.value.filter(
-      (work) => work.year === year // Filter by publication year
+      (work) => work.publication_year === year
     );
     filteredPublications.value = groupByYear(filtered);
   }
@@ -108,7 +132,7 @@ function filterByYear(year) {
 
 function groupByYear(works) {
   const grouped = works.reduce((acc, work) => {
-    const year = work.year;
+    const year = work.publication_year;
     if (!acc[year]) acc[year] = [];
     acc[year].push(work);
     return acc;
@@ -119,13 +143,11 @@ function groupByYear(works) {
     .sort((a, b) => b - a) // Sort years by most recent first
     .reduce((sortedAcc, year) => {
       sortedAcc[year] = grouped[year].sort(
-        (a, b) => b.date - a.date
+        (a, b) => b.publication_year - a.publication_year
       ); // Sort publications by most recent first within each year
       return sortedAcc;
     }, {});
 }
-
-
 </script>
 
 <style scoped>
