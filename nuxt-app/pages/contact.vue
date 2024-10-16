b2fdc48a0b86b5d7a5bfe0e98f669b0febed71b3
Quentin Glorieux <quentin.glorieux@gmail.com>
2024-10-17 00:32:30 +0200

    update contact.vue

--- nuxt-app/pages/contact.vue
+++ nuxt-app/pages/contact.vue
@@ -9,14 +9,14 @@
 
     <CardL
       title="Pierre Buffet"
-      location="pierre.buffet@pasteur.fr"
+      location="pierre.buffet@aphp.fr"
       imageURL="images/steering/pierre.webp"
       abstract="Pierre Buffet is coordinator of the LeishMan group. He is an expert in leishmaniasis, malaria and other parasitic diseases. He has been working on leishmaniasis since the 1980s, and has clinical and microbiological expertise in this field (in vitro and animal models). He works closely with the National Reference Center for Leishmaniasis and the Parasitology Laboratory at Hôpital Cochin. He is also Medical Director of the Institut Pasteur."
     />
 
           <CardR
       title="Clea Melenotte"
-      location="clea.melenotte@pasteur.fr"
+      location="clea.melenotte@aphp.fr"
       imageURL="images/steering/clea.webp"
       abstract="Clea Melenotte is an infectiologist. She co-coordinates the LeishMan network and works closely with the national reference center for leishmaniasis in France. She is developing her expertise in parasitic infections at the Institut Pasteur in Paris, in collaboration with the Assistance Publique des Hôpitaux de Paris."
     />
