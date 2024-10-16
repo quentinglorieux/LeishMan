4995fedc5181e883cc498302945b1aa865e62a8e
Quentin Glorieux <quentin.glorieux@gmail.com>
2024-10-17 00:29:54 +0200

    update 2 files and delete 1 file

--- nuxt-app/components/Footer.vue
+++ nuxt-app/components/Footer.vue
@@ -3,8 +3,8 @@
       <div class="container mx-auto flex justify-between">
         <div>
           <h5 class="font-bold text-lg">Contact Us</h5>
-          <p>1234 Pasteur Ave, Paris, France</p>
-          <p>Email: contact@pasteur-network.org</p>
+          <p></p>
+          <p>Email: contact@leishman-network.org</p>
         </div>
         <div>
           <h5 class="font-bold text-lg">Follow Us</h5>
