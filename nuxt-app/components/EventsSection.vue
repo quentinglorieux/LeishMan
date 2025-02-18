404c16de8f5f5f3cc1f2eea95470b0f16c782452
olivierglrx <olivier.glrx@gmail.com>
2025-02-18 11:20:24 +0100

    eventSection dynamic in index

--- nuxt-app/components/EventsSection.vue
+++ nuxt-app/components/EventsSection.vue
@@ -1,10 +1,21 @@
 <template>
   <section class="py-12">
     <div class="container mx-auto px-10">
-      <h2 class="text-3xl font-bold mb-6">Upcoming Events</h2>
+
+      <h2 v-if="future.length>0" class="text-3xl font-bold mb-6">Upcoming Events</h2>
       <div class="grid md:grid-cols-2 lg:grid-cols-2 gap-6">
         <EventCard
-          v-for="event in eventItems"
+          v-for="event in future"  
+          :key="event.id"
+          :eventItem="event"
+        />
+      </div>
+
+      <h2 class="text-3xl font-bold mt-6 mb-6">Past Events</h2>
+      <div class="grid md:grid-cols-2 lg:grid-cols-2 gap-6">
+        <EventCard
+          v-for="event in past.slice(0,2)"
+          
           :key="event.id"
           :eventItem="event"
         />
@@ -21,23 +32,43 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import EventCard from "@/components/EventCard.vue";
+// import { ref, onMounted } from "vue";
+// import EventCard from "@/components/EventCard.vue";
 
-const eventItems = ref([]);
-const fetchError = ref(null); // Error handling
 
-onMounted(async () => {
-  try {
-    const eventsResponse = await fetch("/data/events.json");
-    if (!eventsResponse.ok) {
-      throw new Error("Failed to fetch events data.");
-    }
-    const eventsData = await eventsResponse.json();
-    eventItems.value = eventsData.meetings.slice(0, 2); // Show the first 3 events
-  } catch (error) {
-    fetchError.value = error.message;
-    console.error("Error fetching events data:", error);
+const eventList = await queryContent("event").sort('date').find();
+
+const future=Array()
+const past= Array()
+const today = new Date()
+eventList.forEach(event => {
+  let dateevent=new Date(event.date) 
+  if(dateevent>today){
+    future.push(event)
   }
+  else{
+    past.push(event)
+  }
+  
 });
+
+
+// const eventItems = ref([]);
+// const fetchError = ref(null); // Error handling
+
+
+
+// onMounted(async () => {
+//   try {
+//     const eventsResponse = await fetch("/data/events.json");
+//     if (!eventsResponse.ok) {
+//       throw new Error("Failed to fetch events data.");
+//     }
+//     const eventsData = await eventsResponse.json();
+//     eventItems.value = eventsData.meetings.slice(0, 2); // Show the first 3 events
+//   } catch (error) {
+//     fetchError.value = error.message;
+//     console.error("Error fetching events data:", error);
+//   }
+// });
 </script>
