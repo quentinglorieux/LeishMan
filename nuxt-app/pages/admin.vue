14508e3046c7d0348167b535b3d60bb1d85ba39e
olivierglrx <olivier.glrx@gmail.com>
2025-06-13 13:03:35 +0200

    admin

---
+++ nuxt-app/pages/admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="sveltia-cms"></div>
+</template>
+
+<script setup>
+useHead({
+  title: 'Content Manager Leishman',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+    { name: 'robots', content: 'noindex' }
+  ],
+  script: [
+    { src: '/admin/dist/sveltia-cms.js' }
+  ]
+})
+</script>
