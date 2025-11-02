6ee29d7650ec2cf5ee15f92e39700f78f7a41e22
Quentin Glorieux <quentin.glorieux@gmail.com>
2025-11-02 16:35:48 +0100

    update [id].vue and config.yml

--- nuxt-app/pages/seminars/[id].vue
+++ nuxt-app/pages/seminars/[id].vue
@@ -1,15 +1,26 @@
 <template>
   <div class="container mx-auto py-8">
     <h1 class="text-3xl font-bold text-center mb-8">
-      {{seminars.date}}  
+      {{ seminars.date }}
     </h1>
     {{ seminars.description }}
-   
+
+    <div v-if="seminars.program_pdf" class="my-6 mx-auto text-center">
+    Download Link: <a
+      :href="seminars.program_pdf"
+      class="mt-6 bg-pasteur-blue hover:bg-pasteur-dark text-white px-6 py-3 rounded-full"
+    >
+      Program in PDF
+    </a>
+    </div>
   </div>
-  <ProgDisplay :programData="seminars.talk" />
+
+  <ProgDisplay v-if="!seminars.program_pdf" :programData="seminars.talk" />
 </template>
 
 <script setup>
 const route = useRoute();
-const seminars = await queryContent("events").where({ title: route.params.id }).findOne();
+const seminars = await queryContent("events")
+  .where({ title: route.params.id })
+  .findOne();
 </script>
